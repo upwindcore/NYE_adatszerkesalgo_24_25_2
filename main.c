1ae4414149425ae3dba153eeb22a458901b6877a
upwindcore <tibor.mercs@protonmail.com>
2025-03-03 13:09:58 +0100

    2025.03.03

--- main.c
+++ main.c
@@ -1,93 +1,40 @@
-/* Írj egy programot, mely képes egy betűsorozatot kezelni, melyben maximum 5, angol nagybetűs abc-be tartozó
- * betű lehet. Valósítsd meg az alábbi "betűk" típust és a függvényeket: */
+// A döntés
 
 /*
-void kiir(betuk b) kiírja a betűket:"XCD"
-int betukSzama(betuk b)
-int ures(betuk b)
-int teli(betuk b)
-int betesz(betuk b, char c)
-char kivesz(betuk b)
-betuk letreHoz()
+Algoritmus Dontes_1(N, X, van)
+    van:=hamis
+    Ciklus i:= 1...N
+        van:= van vagy T(X[i])
+    Ciklus
+Algoritmus
  */
 
-
 #include <stdio.h>
-#include <stdlib.h>
 
-typedef char * betuk;
+int T(int a)
+{
+    int p=1;
+    for(int o=2; o<=a/2; o++)
+        if(a%o==0)p=0;
+        //p=p&&a%o;
+    return p;
+}
 
-void kiir(betuk b)
+void Dontes_1(int N, int *X, int *van)
 {
-    for(int i=0;i<5;i++)
-        if(b[i]!='a')
-            printf("%c",b[i]);
+    *van=0;
+    for (int i; i<N; i++)
+        *van= *van || T(X[i]);
 }
-int betukSzama(betuk b)
-{
-    int db=0;
-    for(int i=0;i<5;i++)
-        if(b[i]!='a') db++;
-    return db;
-}
-int ures (betuk b)
-{
-    return !betukSzama(b);
-    //Első verzió
-    //return betukSzama(b)==0;
-    //Másik verzió
-    /**if(betukSzama(b)==0)
-        //return 1;
-    //else return 0;**/
-}
-int teli (betuk b)
-{
-    return betukSzama(b)==5;
-}
-int betesz (betuk b, char c)
-{
-    if(!teli(b))
-    {
-        int i=0;
-        while(b[i]!='a')
-            i++;
-        printf("\n%d %c %c",i,b[i],c);
-        b[i]=c;
-        return 1;
-    };
-    return 0;
-}
-char kivesz(betuk b)
-{
-
-    int i=0;
-    char c;
-    while(b[i]=='a')
-        i++;
-    c=b[i];b[i]='a';
-    return b[i];
 
 
-}
-betuk letrehoz()
-{
-    betuk d=malloc(5);
-    d[0]='a';d[1]='a';d[2]='a';d[3]='a';d[4]='a';
-    return d;
-}
 int main()
 {
+    int t[]={4, 6, 24, 45, 65};
+    int db=sizeof(t)/sizeof(t[0]);
+    int letezik;
+    Dontes_1(db, t, &letezik);
+    printf(letezik?"Van":"Nincs");
 
-    betuk x=malloc(5);
-    x[0]='a';x[1]='a';x[2]='a';x[3]='a';x[4]='a';
-    kiir(x);
-    printf("\n%d",betukSzama(x));
-    printf(ures(x)?"\nUres":"\nNem ures");
-    printf(teli(x)?"\nTele":"\nNincs tele");
-    //printf(betesz(x,'R')?"\nSiker\n":"\nNincs siker\n");
-    kiir(x);
-    printf("\n%c\n",kivesz(x));
-    kiir(x);
-    free(x);
     return 0;
 }
